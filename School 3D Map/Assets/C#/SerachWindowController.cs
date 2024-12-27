df65745bcfe4cc7c36bd9bde14db4ea0895909f0
Kazuto Nishimura <55263892+Kazu418@users.noreply.github.com>
2024-12-27 18:09:24 +0900

    検索バー・検索UI制作
    変更点
    ・検索バーを引っ張り上げるとメニューが出てくるように改良
    ・場所リストUI構築
    ・検索中のUIの視点移動を制限
    ・画面サイズが変わったときに、UIの見た目が大きくは変わらないようにしてある。
    
    使い方
    ・SearchWindowControllerは、Search_UIにアタッチする。
    ・MainCameraのスクリプト、SmoothCameraRotationのSearchWindowControllerパラメーターにSearch_UIをドラッグ&ドロップする。
    
    一応滑らかに動かなかったら、Project→UI→SearchBar.uxmlファイルを開き、#Searchのinspectorの、transition animationsの項目を、all,0.4,Ease Out Cubic,0に設定すると滑らかにいくはず。
    一応Notionにそれっぽい画像を貼っておく

--- School 3D Map/Assets/C#/SerachWindowController.cs
+++ School 3D Map/Assets/C#/SerachWindowController.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.Design.Serialization;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,32 +8,142 @@ public class SerachWindowController:MonoBehaviour
     private bool isFolded = true;
     private VisualElement root;
     public VisualElement search;
+    public VisualElement view;
+    private VisualElement searchBar;
+    private VisualElement searchButton;
     public TextField textField;
 
+
+    private Vector2 touchStartPosition;
+    private float SwipeThreshold = 0.5f;
+    public bool isDragging = false;
+    private float windowStartPositionY;
+    private float screenHeight;
+
     void OnEnable(){
+
         root = GetComponent<UIDocument>().rootVisualElement;
         textField = root.Q<TextField>("Text_Input");
         search = root.Q<VisualElement>("Search");
+        view = root.Q<VisualElement>("View");
+        searchBar = root.Q<VisualElement>("Search_Bar");
+        searchButton = root.Q<VisualElement>("Search_Button");
 
         if(textField == null||search==null){
             Debug.LogError("UI Document,Search内のUI要素が見つかりません。");
         }
 
         textField.RegisterCallback<FocusEvent>(OnFocus);
-        textField.RegisterCallback<BlurEvent>(OnBlur);
 
-        search.AddToClassList("Folded");
+        search.RegisterCallback<PointerDownEvent>(OnPointerDown);
+        view.RegisterCallback<PointerMoveEvent>(OnPointerMove);
+        view.RegisterCallback<PointerUpEvent>(OnPointerUp);
+        view.RegisterCallback<PointerLeaveEvent>(OnPointerLeave);
+        view.RegisterCallback<PointerDownEvent>(evt=>{
+            if(!isFolded){
+                hideWindow();
+            }
+            evt.StopPropagation();
+        });
+
+        //Uiサイズ制御
+        searchBar.RegisterCallback<GeometryChangedEvent>(evt =>{
+            float searchBarWidth = searchBar.resolvedStyle.width;
+            float borderRadiusRate = 0.05f;
+            searchBar.SetBorderRadius(searchBarWidth*borderRadiusRate);//拡張メゾット
+            //searchBarWidthは578px
+
+            float searchButtonSizeRate = 0.043f;
+            searchButton.style.width = searchBarWidth*searchButtonSizeRate;
+            searchButton.style.height = searchBarWidth*searchButtonSizeRate;
+
+            float searchButtonRadiusRate = 0.01f;
+            searchButton.SetBorderRadius(searchBarWidth*searchButtonRadiusRate);
+        });
+        //レイアウトセット前に画面サイズを設定しようとすると、正確な値が返ってこない。
+        textField.RegisterCallback<GeometryChangedEvent>(evt=>{
+           float textFieldWidth = textField.resolvedStyle.width;
+           float radiusRate = 0.036f;
+           textField.SetBorderRadius(textFieldWidth*radiusRate);
+
+           float fontSizeRate = 0.03f; 
+           textField.style.fontSize = textFieldWidth*fontSizeRate;
+        });
+
     }
 
+    //これより下、RegisterCallbackによって定義され、（ユーザーが指定の動作をした場合の）実行する関数
     private void OnFocus(FocusEvent evt){
-        search.RemoveFromClassList("Folded");
-        search.AddToClassList("unFolded");
-    }
-    private void OnBlur(BlurEvent evt){
-        search.RemoveFromClassList("unFolded");
-        search.AddToClassList("Folded");
+        showWindow();
     }
 
-    
+    private void OnPointerDown(PointerDownEvent evt){
+        touchStartPosition = evt.position;
+        windowStartPositionY = search.resolvedStyle.top;
+        isDragging = true;
+        evt.StopPropagation();
+    }
+    private void OnPointerMove(PointerMoveEvent evt){
+        if(!isDragging) return;
+        float deltaY = evt.position.y - touchStartPosition.y;
 
+        search.style.top = new StyleLength(windowStartPositionY + deltaY);
+    }
+    private void OnPointerUp(PointerUpEvent evt){
+        if(!isDragging) return;
+        float deltaY = evt.position.y - touchStartPosition.y;
+
+        moveToFixedPosition(deltaY);
+        
+    }
+    private void OnPointerLeave(PointerLeaveEvent evt){
+        float deltaY = evt.position.y - touchStartPosition.y;
+
+        moveToFixedPosition(deltaY);
+    }
+
+//これより下、実行される関数の中で使われる
+    private void moveToFixedPosition(float deltaY){
+        isDragging = false;
+
+        if(Mathf.Abs(deltaY)>screenHeight*SwipeThreshold){
+            if(deltaY < 0){
+                showWindow();
+                print("展開、新たに");
+            }
+            else{
+                hideWindow();
+                print("折りたたみ、新たに");
+            }
+        }
+        else{
+            if(isFolded){
+                hideWindow();
+                print("折りたたみ、元に戻る");
+            }
+            else{
+                showWindow();
+                print("展開、元に戻る");
+            }
+        }
+    }
+    private void showWindow(){
+        search.style.top = new StyleLength(Length.Percent(8));
+        isFolded = false;
+    }
+    private void hideWindow(){
+        search.style.top = new StyleLength(Length.Percent(92));
+        isFolded = true;
+    }
+
+}
+
+public static class VisualElementExtensions{
+        public static void SetBorderRadius(this VisualElement element, float radius)
+    {
+        element.style.borderTopLeftRadius = radius;
+        element.style.borderTopRightRadius = radius;
+        element.style.borderBottomLeftRadius = radius;
+        element.style.borderBottomRightRadius = radius;
+    }
 }
